bfcb69c23f35f3048b72c6cf84059882094b1fa6
DerekFurstPitt <drf57@pitt.edu>
2025-12-08 15:10:11 -0500

    update toml and renamed to jobq

---
+++ src/atlas_consortia_jobq/__init__.py
@@ -0,0 +1,4 @@
+from atlas_consortia_jobq.queue import SearchQueue
+
+__version__ = "0.1.0"
+__all__ = ["SearchQueue"]
--- src/atlas_consortia_queue/__init__.py
+++
@@ -1,4 +0,0 @@
-from atlas_consortia_queue.queue import SearchQueue
-
-__version__ = "0.1.0"
-__all__ = ["SearchQueue"]
